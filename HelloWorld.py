f3ed7e06e7ee06cacc6012ff0d4a74ab556e655c
Unknown <SnowyValkyrie@gmail.com>
2018-05-04 13:35:22 -0400

    Simple Hello World
    A quick tidbit using input and spitting it back out.

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+print('------------------------')
+print('       Hello App')
+print('------------------------')
+print()
+
+user_name = input('What is your name? ')
+print("Hello " + user_name + "! hope you enjoy your time in python!")
